55e3f3135391638f3aa5670d9657f06dd9106e91
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-03 23:48:46 -0500

    New Reposity

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,4 @@
+#include <math.h>
+#include <stdio.h>
+#include <complex.h>
+
---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct complex - complex
+ * @a: double 1
+ * @b: double 2
+ */
+struct complex
+{
+	double a;
+	double b;
+};
+void display_complex_number(complex c);
+
+#endif
