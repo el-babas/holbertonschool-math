48c027d87b9aa5fc3a1e24ab681a6ebd764d1f9f
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-11 17:41:18 -0500

    0-Task

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,8 +6,22 @@
  */
 void display_complex_number(complex c)
 {
+	char s = (c.im < 0) ? '-' : '+';
+	double s_im = (c.im < 0) ? -(c.im) : c.im;
+
 	if (c.im == 0)
+	{
 		printf("%.0f\n", c.re);
-	else
-		printf("%.0f + %.0fi\n", c.re, c.im);
+	}
+	else if (c.re == 0)
+	{
+		if (c.im > 0)
+			printf("%.0fi\n", s_im);
+		else
+			printf("%c %.0fi\n", s, s_im);
+	}
+	else if (c.re != 0 && c.im != 0)
+	{
+		printf("%.0f %c %.0fi\n", c.re, s, s_im);
+	}
 }
