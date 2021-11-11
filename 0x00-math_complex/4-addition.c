5970b3a1aa11a70d7273fbb42df40f43a02b96ba
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-11 18:48:58 -0500

    4-Task

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * addition - function that performs the addition operation to complex numbers.
+ * @c1: complex 1
+ * @c2: complex 2
+ * @c3: result 	in complex 3
+ */
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
