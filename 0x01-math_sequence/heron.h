625cd809d31ce08a62b731c3655e9958a785a298
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-26 22:10:17 -0500

    0-Task

--- 0x01-math_sequence/heron.h
+++ 0x01-math_sequence/heron.h
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct cell - list of results
+ * @elt: number
+ * @next: point to the next node
+ */
+typedef struct cell
+{
+	int elt;
+	struct cell *next;
+} t_cell;
+
 t_cell *heron(double p, double x0);
 
 #endif
